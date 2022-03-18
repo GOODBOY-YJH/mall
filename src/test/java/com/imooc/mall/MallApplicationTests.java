2ee3ca4bc8b3b402368c91af181aa22b17330015
GOODBOY-YJH <1613777456@qq.com>
2022-03-18 10:42:42 +0800

    数据库配置

--- src/test/java/com/imooc/mall/MallApplicationTests.java
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -1,18 +1,27 @@
 package com.imooc.mall;
 
 
+import com.imooc.mall.dao.CategoryMapper;
+import com.imooc.mall.pojo.Category;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class MallApplicationTests {
+public class MallApplicationTests {
+    public MallApplicationTests(){}
+
+    @Autowired
+    private CategoryMapper categoryMapper;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        Category category = categoryMapper.findById(100002);
+        System.out.println(category);
     }
 
 }
