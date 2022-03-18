32b37ce6249832b7064e2aaf96be14d1a06214a6
GOODBOY-YJH <1613777456@qq.com>
2022-03-18 11:20:16 +0800

    测试执行

--- src/test/java/com/imooc/mall/MallApplicationTests.java
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -1,11 +1,8 @@
 package com.imooc.mall;
 
 
-import com.imooc.mall.dao.CategoryMapper;
-import com.imooc.mall.pojo.Category;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,18 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MallApplicationTests {
     public MallApplicationTests(){}
 
-    @Autowired
-    private CategoryMapper categoryMapper;
-
     @Test
     public void contextLoads() {
-        Category category = categoryMapper.findById(100002);
-        System.out.println(category);
+
     }
 
-    @Test
-    public void queryByIdTest() {
-        Category category = categoryMapper.queryById(100002);
-        System.out.println(category);
-    }
 }
--- src/test/java/com/imooc/mall/dao/CategoryMapperTest.java
+++ src/test/java/com/imooc/mall/dao/CategoryMapperTest.java
@@ -1,16 +1,28 @@
 package com.imooc.mall.dao;
 
+import com.imooc.mall.pojo.Category;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryMapperTest  {
 
-public class CategoryMapperTest {
+    @Autowired
+    private CategoryMapper categoryMapper;
 
     @Test
-    public void findById() {
+    public void contextLoads() {
+        Category category = categoryMapper.findById(100002);
+        System.out.println(category);
     }
 
     @Test
-    public void queryById() {
+    public void queryByIdTest() {
+        Category category = categoryMapper.queryById(100002);
+        System.out.println(category);
     }
 }
