e8da9a9894a7ff8fa68091581af0337a7a0e160d
GOODBOY-YJH <1613777456@qq.com>
2022-03-19 17:20:19 +0800

    类目功能开发

---
+++ src/main/java/com/imooc/mall/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.imooc.mall.service.impl;
+
+import com.imooc.mall.dao.CategoryMapper;
+import com.imooc.mall.pojo.Category;
+import com.imooc.mall.service.ICategoryService;
+import com.imooc.mall.vo.CategoryVo;
+import com.imooc.mall.vo.ResponseVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.imooc.mall.consts.MallConst.ROOT_PARENT_ID;
+
+@Service
+public class CategoryServiceImpl implements ICategoryService {
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public ResponseVo<List<CategoryVo>> selectAll() {
+        List<CategoryVo> categoryVoList = new ArrayList<>();
+        List<Category> categories = categoryMapper.selectAll();
+        // 查出parent_id=0的数据
+        for (Category category : categories) {
+            if(category.getParentId().equals(ROOT_PARENT_ID)) {
+                CategoryVo categoryVo = new CategoryVo();
+                BeanUtils.copyProperties(category, categoryVo);
+                categoryVoList.add(categoryVo);
+            }
+        }
+        return ResponseVo.success(categoryVoList);
+    }
+}
