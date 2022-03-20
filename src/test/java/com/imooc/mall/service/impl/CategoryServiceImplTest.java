c9caf0c86d3dcdf6edea56e39f7799d8d274c16c
GOODBOY-YJH <1613777456@qq.com>
2022-03-20 08:48:46 +0800

    分页模块开发

--- src/test/java/com/imooc/mall/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/imooc/mall/service/impl/CategoryServiceImplTest.java
@@ -4,6 +4,7 @@ import com.imooc.mall.enums.ResponseEnum;
 import com.imooc.mall.service.ICategoryService;
 import com.imooc.mall.vo.CategoryVo;
 import com.imooc.mall.vo.ResponseVo;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,11 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
+@Slf4j
 public class CategoryServiceImplTest {
 
     @Autowired
@@ -26,6 +30,13 @@ public class CategoryServiceImplTest {
     public void selectAll(){
         ResponseVo<List<CategoryVo>> listResponseVo = categoryService.selectAll();
         Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), listResponseVo.getStatus());
+    }
+
+    @Test
+    public void findSubCategoryId(){
+        Set<Integer> set = new HashSet<>();
+        categoryService.findSubCategoryId(100001, set);
+        log.info("set={}",set);
     }
 
 }
---
+++ src/test/java/com/imooc/mall/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.imooc.mall.service.impl;
+
+import com.github.pagehelper.PageInfo;
+import com.imooc.mall.dao.ProductMapper;
+import com.imooc.mall.enums.ResponseEnum;
+import com.imooc.mall.service.IProductService;
+import com.imooc.mall.vo.ResponseVo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Transactional
+public class ProductServiceImplTest {
+    @Autowired
+    private IProductService productService;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Test
+    public void list() {
+        ResponseVo<PageInfo> list = productService.list(null, 1, 2);
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), list.getStatus());
+    }
+}
